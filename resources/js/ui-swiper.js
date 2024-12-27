188bb04141fdcd1f9a1bf0702b91e9270e81f3fe
jojosg <jojosg93@naver.com>
2024-12-27 17:37:47 +0900

    SM 프로젝트 뼈대구축

---
+++ resources/js/ui-swiper.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".slide-intro", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".slide-intro .swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".slide-studio", {
+  loop: true,
+  slidesPerView: 1.2,
+  spaceBetween: 20,
+  pagination: {
+    el: ".slide-studio .swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2.2,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
